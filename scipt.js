c3b7218f310567fb98339ac98300ce7b7386c5de
Gabriel <160670197+Gdhjjsjhd@users.noreply.github.com>
2024-09-02 08:37:32 -0300

    Add files via upload

--- scipt.js
+++ scipt.js
@@ -20,4 +20,37 @@ function converter(){
         fatorConversao = 1000;
     }
 
+
+    var valorConvresor = valor * fatorConversao;
+
+    resultado.innerHtml = valor + " "  + deUnidade + "é igual a" + valorConvresor + " " + paraUnidade;
+}
+
+
+function converterPeso(){
+    var valor = parseFloat(document.getElementById('valor').value)
+    var deUnidade = parseFloat(document.getElementById('dePeso').value)
+    var paraUnidade = parseFloat(document.getElementById('paraPeso').value)
+    var resultado = parseFloat(document.getElementById('resultado').value)
+
+    var valorConvertido;
+
+    if(deUnidade === "g" && paraUnidade === "kg"){
+        valorConvertido = valor / 1000;
+    } else if(deUnidade === "g" && paraUnidade === "lb"){
+        valorConvertido = valor / 453.592;
+    } else if(deUnidade === "kg" && paraUnidade === "g"){
+        valorConvertido = valor * 1000
+    } else if(deUnidade === "kg" && paraUnidade === "lb"){
+        valorConvertido = valor * 2.205;
+    } else if(deUnidade === "lb" && paraUnidade === "g"){
+        valorConvertido = valor * 453.592;
+    } else if(deUnidade === "lb" && paraUnidade === "kg"){
+        valorConvertido = valor / 2.205;
+    }
+
+
+    var valorConvresor = valor * fatorConversao;
+
+    resultado.innerHtml = valor + " "  + deUnidade + "é igual a" + valorConvresor + " " + paraUnidade;
 }
